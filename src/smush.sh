2ad9f9acfb31006906a2dadf9bb75e119eb6f272
Andreas Strauman <andreas@strauman.net>
2018-03-30 21:40:51 +0200

    More updates to docs

--- src/smush.sh
+++ src/smush.sh
@@ -12,38 +12,16 @@ cd $docDir
 
 echo "Building documentation"
 latexmk -pdf documentation.tex -outdir=bin --shell-escape
-cd bin/
-texclean
-cd ../
 cp bin/documentation.pdf ../manual.pdf
 
 cd $sourceDir
 
 cat packagehead.tex > $outfile
 latexpand --empty-comments $infile >> $outfile
+./formatPackage.pl
+rm exbank.sty
+mv exbank.stripped.sty exbank.sty
 
-# Remove comments that starts but has no content following it
-perl -i -pe 's/^\s*[%]+\s*$//g' $outfile
-# Remove all blank lines
-sed -i.bak -E '/./!d' $outfile
-# Remove all makeatletter and makeatothers (not needed for .sty)
-perl -i -pe 's/(\\makeatletter|\\makeatother)//g' $outfile
-# Remove all indentation
-# sed -i -E 's/^[[:blank:]]*//' $outfile
-# Remove all newlines
-# perl -0777 -i.bak -pe 's/\n/ /g' $outfile
-# Remove all spaces before commands
-perl -i -pe 's/(\\[a-z]+|\})\s+(\\[a-z]+)/\1\2/g' $outfile
-# Remove all spaces at beginning of groups
-perl -i -pe 's/\{\s+/\{/g' $outfile
-# Remove all spaces at end of groups
-perl -i -pe 's/\s+\}/\}/g' $outfile
-# Add comment to top
-# cp $outfile tmpOut
-# echo "%% Source and documentation at https://github.com/Strauman/Handin-LaTeX" > $outfile
-# cat tmpOut >> $outfile
-## Generate dependencylist from packages
-# perl -pe '/^\\usepackage\{([^\{]+)\}/\1/' packages.tex
 
 ## Create quickstart
 rm quickstart.zip
@@ -51,8 +29,6 @@ zip quickstart.zip example.tex $outfile -r ../exercises
 zip example.zip example.tex -r ../exercises
 zip exbankCTAN.zip ../manual.pdf README.txt $outfile -r ../exercises
 mv exbankCTAN.zip ../
-# perl -pe 's/^(?!\\usepackage).*//; s/^\\usepackage(?:\[[^\]]+\])?\{([^\{]+)\}/\1/;s/^\n//g' packages.tex > dependencies.txt
-# perl -i -pe 's/\n/,/g' dependencies.txt
 
 mv quickstart.zip ../
 rm $outfile.bak
@@ -61,3 +37,29 @@ rm ../$outfile
 cp example.tex ../
 cp $outfile ../
 cp $outfile /Users/Andreas/Documents/uit/fag/TA/FYS-1002/Oppgaver\ FYS-1002/$outfile
+
+# perl -pe 's/^(?!\\usepackage).*//; s/^\\usepackage(?:\[[^\]]+\])?\{([^\{]+)\}/\1/;s/^\n//g' packages.tex > dependencies.txt
+# perl -i -pe 's/\n/,/g' dependencies.txt
+
+# Remove comments that starts but has no content following it
+# perl -i -pe 's/^\s*[%]+\s*$//g' $outfile
+# Remove all blank lines
+# sed -i.bak -E '/./!d' $outfile
+# Remove all makeatletter and makeatothers (not needed for .sty)
+# perl -i -pe 's/(\\makeatletter|\\makeatother)//g' $outfile
+# Remove all indentation
+# sed -i -E 's/^[[:blank:]]*//' $outfile
+# Remove all newlines
+# perl -0777 -i.bak -pe 's/\n/ /g' $outfile
+# Remove all spaces before commands
+# perl -i -pe 's/(\\[a-z]+|\})\s+(\\[a-z]+)/\1\2/g' $outfile
+# Remove all spaces at beginning of groups
+# perl -i -pe 's/\{\s+/\{/g' $outfile
+# Remove all spaces at end of groups
+# perl -i -pe 's/\s+\}/\}/g' $outfile
+# Add comment to top
+# cp $outfile tmpOut
+# echo "%% Source and documentation at https://github.com/Strauman/Handin-LaTeX" > $outfile
+# cat tmpOut >> $outfile
+## Generate dependencylist from packages
+# perl -pe '/^\\usepackage\{([^\{]+)\}/\1/' packages.tex
