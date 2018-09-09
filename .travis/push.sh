4c023600a45d89b0c16a5657197797cddfbfab38
Andreas Strauman <andreas@strauman.net>
2018-09-09 18:07:03 +0200

    Auth origin

--- .travis/push.sh
+++ .travis/push.sh
@@ -14,9 +14,9 @@ commit_pdfs() {
 }
 
 upload_files() {
-  # git remote add origin-login https://${GH_TOKEN}@github.com/Strauman/exercisebank.git
-  # git push --set-upstream origin-login travisbranch
-  git push --set-upstream origin travisbranch
+  git remote add origin-login https://${GH_TOKEN}@github.com/Strauman/exercisebank.git
+  git push --set-upstream origin-login travisbranch
+  # git push --set-upstream origin travisbranch
 }
 
 setup_git
