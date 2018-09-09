8c6cd179a5e272a3cfd7e0197cad33105687bf13
Andreas Strauman <andreas@strauman.net>
2018-09-09 17:43:53 +0200

    Push update

--- .travis/push.sh
+++ .travis/push.sh
@@ -8,7 +8,7 @@ setup_git() {
 
 commit_website_files() {
   git checkout -b travisbranch
-  git add . *.pdf
+  git add tests/*.pdf
   echo `git status`
   git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
 }
